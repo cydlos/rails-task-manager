e1c48c9469e375f058067308f7f87bc9a7c5fbba
cyd losekann <cydnel@gmail.com>
2022-08-16 16:57:10 -0300

    completed, poor style

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,43 @@
+class TasksController < ApplicationController
+  before_action :get_task, only: %i[show edit update destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task.update(tasks_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def get_task
+    @task = Task.find(params[:id])
+  end
+
+  def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,18 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'articles#index'
-  get 'tasks/:id', to: 'tasks#index'
+  get 'tasks', to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  get 'task/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  patch 'task/:id', to: 'tasks#update'
+
+  get 'task/:id', to: 'tasks#show', as: 'task'
+
+  delete 'task/:id', to: 'tasks#destroy', as: 'task_delete'
+  # resources :tasks
 end
